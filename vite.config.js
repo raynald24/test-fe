0a4d13885333bb775ca512da872900ea85004c57
unknown <raynaldkalee24@gmail.com>
2025-08-04 06:39:04 +0700

    Fix: Corrected base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/test-fe/', 
+  base: '', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
